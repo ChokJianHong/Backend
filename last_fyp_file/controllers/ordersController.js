8eb263ec7139719a76a693652bd1027767322c9f
WilsonLo99 <loweiseng99@gmail.com>
2024-10-25 22:50:40 +0800

    update

--- last_fyp_file/controllers/ordersController.js
+++ last_fyp_file/controllers/ordersController.js
@@ -70,7 +70,8 @@ function viewRequestDetail(req, res) {
     c.email as customer_email,
     c.auto_gate_brand as customer_auto_gate_brand,
     c.alarm_brand as customer_alarm_brand,
-    c.warranty as customer_warranty
+    c.auto_gate_warranty as customer_auto_gate_warranty
+    c.alarm_warranty as customer_alarm_warranty
     FROM 
     ordertable
     JOIN 
@@ -88,7 +89,8 @@ function viewRequestDetail(req, res) {
     c.email as customer_email,
     c.auto_gate_brand as customer_auto_gate_brand,
     c.alarm_brand as customer_alarm_brand,
-    c.warranty as customer_warranty, 
+    c.auto_gate_warranty as customer_auto_gate_warranty,
+    c.alarm_warranty as customer_alarm_warranty,
     t.name AS technician_name,
     t.phone_number AS technician_phone_number,
     t.specialization AS technician_specialization
@@ -140,7 +142,8 @@ function viewRequestDetail(req, res) {
         phone: results[0].customer_phone_number,
         autogateBrand: results[0].customer_auto_gate_brand,
         alarmBrand: results[0].customer_alarm_brand,
-        warranty: results[0].customer_warranty,
+        autogateWarranty: results[0].customer_auto_gate_warranty,
+        alarmWarranty: results[0].customer_alarm_warranty,
       },
       technician: {
         name: results[0]?.technician_name || "",
@@ -169,7 +172,8 @@ function getOrderDetail(req, res) {
       c.email as customer_email,
       c.auto_gate_brand as customer_auto_gate_brand,
       c.alarm_brand as customer_alarm_brand,
-      c.warranty as customer_warranty
+      c.auto_gate_warranty as customer_auto_gate_warranty,
+      c.alarm_warranty as customer_alarm_warranty
     FROM 
       ordertable
     JOIN 
@@ -224,7 +228,8 @@ function getOrderDetail(req, res) {
         phone: results[0].customer_phone_number,
         autogateBrand: results[0].customer_auto_gate_brand,
         alarmBrand: results[0].customer_alarm_brand,
-        warranty: results[0].customer_warranty,
+        autogateWarranty: results[0].customer_auto_gate_warranty,
+        alarmWarranty: results[0].customer_alarm_warranty,
       },
       technician: {
         name: results[0]?.technician_name || "",
@@ -828,7 +833,8 @@ function viewOrdersDetail(req, res) {
       c.email as customer_email,
       c.auto_gate_brand as customer_auto_gate_brand,
       c.alarm_brand as customer_alarm_brand,
-      c.warranty as customer_warranty
+      c.auto_gate_warranty as customer_auto_gate_warranty,
+      c.alarm_warranty as customer_alarm_warranty
       
     FROM 
       ordertable
@@ -874,7 +880,8 @@ function viewOrdersDetail(req, res) {
         phone: results[0].customer_phone_number,
         autogateBrand: results[0].customer_auto_gate_brand,
         alarmBrand: results[0].customer_alarm_brand,
-        warranty: results[0].customer_warranty,
+        autogateWarranty: results[0].customer_auto_gate_warranty,
+        alarmWarranty: results[0].customer_alarm_warranty,
       },
     };
 
